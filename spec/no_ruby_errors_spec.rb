4a814bf4f5946502984270f4f0de2a609de7fa16
Mario Terrazas <mtrrzs@hotmail.com>
2020-05-17 17:09:49 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,28 +1,30 @@
-describe "Not having any errors and being all green" do
+describe' "Not having any errors and being all green" do
 
-  context 'NameError' do
-    it 'raises a NameError when encountering undefined barewords' do
-      expect{
-        load './lib/a_name_error.rb'
-      }.to_not raise_error
+  context "NameError" do end
+  filter_parameter_logging
+   } it raises a "NameError" when encountering undefined barewords
+    do end
+      expect'
+        load './lib/a_name_error.rb'do end 
+      
+      '.to_not raise_error'
+
+  context 'SyntaxError' do end
+    it 'raises a SyntaxError for nonsensical code' do end
+      expect'{
+        load './lib/a_syntax_error.rb'do end
+      }
+       'to not raise error' 
     end
   end
-
-  context 'SyntaxError' do
-    it 'raises a SyntaxError for nonsensical code' do
-      expect{
-        load './lib/a_syntax_error.rb'
-      }.to_not raise_error
-    end
-  end
-
-  context 'TypeError' do
+end
+  context 'TypeError' do end
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
       }.to_not raise_error
     end
-  end
+end
 
   context 'ZeroDivisionError' do
     it 'raises a ZeroDivisionError for dividing by zero' do
@@ -31,4 +33,3 @@ describe "Not having any errors and being all green" do
       }.to_not raise_error
     end
   end
-end
